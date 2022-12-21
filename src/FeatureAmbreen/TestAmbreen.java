dba138a93be7625f048da8f4f264fd18ebcef53a
Ambreen <ambreen13@outlook.com>
2022-12-21 22:05:07 +0000

    My first Commit

---
+++ src/FeatureAmbreen/TestAmbreen.java
@@ -0,0 +1,10 @@
+package FeatureAmbreen;
+
+public class TestAmbreen {
+    public static void main(String[] args) {
+        // First Commit
+        System.out.println("FIRST Group WORK");
+
+
+    }
+}
