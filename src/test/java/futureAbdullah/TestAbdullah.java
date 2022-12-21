e2e64c2e0b5ef21c3ebd456e0ae80ad61c74e9d5
abdmacbookair <abdullahsagir.elt@gmail.com>
2022-12-21 22:15:18 +0000

    update3

--- src/test/java/futureAbdullah/TestAbdullah.java
+++ src/test/java/futureAbdullah/TestAbdullah.java
@@ -4,5 +4,6 @@ public class TestAbdullah {
     public static void main(String[] args) {
         //firs commend
         System.out.println("hello word");
+        System.out.println("");
     }
 }
