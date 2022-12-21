9c82c9381e9e0b5229c9a8f582b3ba30ef0a562f
ArzuKizil <arzukizilari@gmail.com>
2022-12-21 22:02:24 +0000

    Updated TestArzu class

--- src/test/java/FeatureArzu/TestArzu.java
+++ src/test/java/FeatureArzu/TestArzu.java
@@ -3,5 +3,6 @@ package FeatureArzu;
 public class TestArzu {
     public static void main(String[] args) {
         System.out.println("Hello friends? How you doin?");
+        System.out.println("Good luck with the new project");
     }
 }
