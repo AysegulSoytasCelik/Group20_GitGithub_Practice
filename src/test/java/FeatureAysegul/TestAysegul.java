699d8bd54912792a9c0a5cad4ee4095bd3c47a20
Aysegul Soytas Celik <aysegulsoytaszb@gmail.com>
2022-12-21 22:05:25 +0000

    added another commit

--- src/test/java/FeatureAysegul/TestAysegul.java
+++ src/test/java/FeatureAysegul/TestAysegul.java
@@ -3,8 +3,15 @@ package FeatureAysegul;
 public class TestAysegul {
 
     // First commit
+
+    //Java is a magic.
+
     public static void main(String[] args) {
         System.out.println("Hello everyone");
+
+        System.out.println("Wooden Spoon");
+
+
     }
 
 }
